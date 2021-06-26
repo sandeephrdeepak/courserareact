b8f42df4649f2505b7abef4d0faa36e2dac9d69d
Sandeep Kumar <sandeephrdeepak@gmail.com>
2021-06-26 18:59:54 +0530

    before header and footer commit

---
+++ src/components/MainComponent.js
@@ -0,0 +1,36 @@
+
+import React, { Component } from 'react';
+import { DISHES } from '../shared/dishes';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
+import Dishdetail from './components/DishdetailComponent';
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES,
+            selectedDish: null
+        };
+    }
+    onDishSelect(dishId) {
+        this.setState({ selectedDish: dishId });
+    }
+    render() {
+        return (
+            <div>
+                <Navbar dark color="primary">
+                    <div className="container">
+                        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+                    </div>
+                </Navbar>
+                <Menu dishes={this.state.dishes}
+                    onClick={(dishId) => this.onDishSelect(dishId)} />
+                    <Dishdetail dish={this.state.dishes.filter((dish)=>dish.id===this.state.selectedDish)[0]}></Dishdetail>
+            </div>
+        );
+    }
+}
+
+
+export default Main;
